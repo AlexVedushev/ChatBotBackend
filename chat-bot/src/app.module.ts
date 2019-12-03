9dbcdbf156d5eaca6d65bf5b62062f7b29b1697c
Alexey Vedushev <alexey.vedushev@omega-r.com>
2019-12-03 17:06:08 +0300

    commit

--- chat-bot/src/app.module.ts
+++ chat-bot/src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WebhookController } from './webhook/webhook.controller'
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(),],
   controllers: [AppController, WebhookController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly connection: Connection) {}
+}
